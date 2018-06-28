6ff4ce93804d6dd63126582fc0d7d8e1434882e6
Bob Brown <robertbernardbrown@gmail.com>
2018-06-27 21:46:02 -0400

    adds more starter files

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Restaurant Bucketlist</h1>
+            <hr className="my-4"/>
+            <p>Sign-in and make a list of restaurants to visit!</p>
+        </div>
+    </div>
+)
+
+export default Header;
