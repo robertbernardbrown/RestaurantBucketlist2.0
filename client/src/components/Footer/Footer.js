ca236d9c94bd63ebcb9ce52f3240cce1b001d9f3
Bob Brown <robertbernardbrown@gmail.com>
2018-06-30 14:38:18 -0400

    fixes footer to bottom of page

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => (
-    <footer className="footer">
+    <footer className="footer navbar-fixed-bottom">
         <span className="footer-span">BobBrown 2017</span>
     </footer>
 )
