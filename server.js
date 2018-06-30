e58aa5b52f860ae89cbf99fa8f330b00d6d5e484
Bob Brown <robertbernardbrown@gmail.com>
2018-06-30 14:42:44 -0400

    adds bodyparser middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,10 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+//BODY-PARSER MIDDLEWARE
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Send every request to the React app
 // Define any API routes before this runs
